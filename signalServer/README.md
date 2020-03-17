065e2940d10ec0fa38d1f7e34ff0d8bc09275e6f
Fl1nn <33037461+Fl1nn@users.noreply.github.com>
2020-03-17 09:27:26 +0700

    Add files via upload

---
+++ signalServer/README.md
@@ -0,0 +1,6 @@
+# Signal-Server
+
+## Usage
+- open cmd
+- `npm install`
+- `node src/app`
---
+++ signalServer/src/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
+const path = require('path')
+require('dotenv').config()
+
+const getCallback = require('./middlewares/getCallback')
+const listenCallback = require('./middlewares/listenCallback')
+
+const app = express()
+const options = {
+  //key: fs.readFileSync('./fake-keys/privatekey.pem'),
+  //cert: fs.readFileSync('./fake-keys/certificate.pem'),
+}
+
+let server
+if (process.env.LOCAL) {
+  server = https.createServer(options, app)
+} else {
+  server = http.createServer(app)
+}
+const io = require('socket.io')(server)
+const ioObject = require('./middlewares/ioCallback')(io)
+const serverPort = (process.env.PORT || 80)
+
+app.use(express.static(path.join(__dirname, '../test')))
+app.get('/', getCallback)
+io.on('connection', (io) => {
+  const master = "webRTC-signal-sever-key";
+  if (master === process.env.MASTER_KEY) {
+   // console.log(`Socket id12: ${io.id}`);
+    ioObject.ioCallback(io)
+   // console.log(`Socket id12: ${io.id}`);
+  } else {
+    console.log('Unauthenticated')
+  }
+})
+
+server.listen(serverPort, listenCallback(serverPort))
